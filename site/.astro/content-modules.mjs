566b2b5e7f00b9827008e57bcb1e9d797d69265d
Ronald Tse <ronald.tse@ribose.com>
2026-08-06 01:28:40 +0800

    drop the astro build artifacts from tracking (generated on every build)

--- site/.astro/content-modules.mjs
+++
@@ -1,8 +0,0 @@
-
-export default new Map([
-["src/content/docs/sst/01-twin-endpoint.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fsst%2F01-twin-endpoint.mdx&astroContentModuleFlag=true")],
-["src/content/docs/sst/02-instrument-kinds.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fsst%2F02-instrument-kinds.mdx&astroContentModuleFlag=true")],
-["src/content/docs/sst/03-driving-a-test-run.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fsst%2F03-driving-a-test-run.mdx&astroContentModuleFlag=true")],
-["src/content/docs/sst/04-running-standalone.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fsst%2F04-running-standalone.mdx&astroContentModuleFlag=true")],
-["src/content/docs/sst/overview.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fsst%2Foverview.mdx&astroContentModuleFlag=true")]]);
-		
