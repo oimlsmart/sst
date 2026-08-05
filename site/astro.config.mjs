986b63398bb8563f796b85271b28629e93560af6
Ronald Tse <ronald.tse@ribose.com>
2026-08-05 09:36:54 +0800

    the SST minisite (TODO.components/09): /sst served from this repo (site/), the logo hero, the kinds, running a sim

---
+++ site/astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from 'astro/config'
+
+export default defineConfig({
+  site: 'https://www.oimlsmart.org',
+  base: '/sst',
+  output: 'static',
+  outDir: '../dist',
+})
